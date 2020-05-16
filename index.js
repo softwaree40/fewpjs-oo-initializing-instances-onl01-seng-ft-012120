294364fc277939d6986f6f652fc87a6906ce48c9
github username <github email address>
2020-05-16 01:35:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your code here
+class Breakfast{
+
+    constructor(food,drink){
+
+     this.food = food;
+     this.drink = drink
+
+ }
+
+
+}
+class Lunch{
+
+constructor(salad,soup,drink){
+  this.soup = soup
+  this.drink = drink
+  this.salad = salad
+
+}
+
+
+
+}
+class Dinner{
+constructor(salad,soup, entree, _dessert){
+
+ this.salad = salad
+ this.soup = soup 
+ this.entree = entree
+ this._dessert = _dessert
+}
+
+
+
+
+}
